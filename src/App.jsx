25f4555a495762caeca99361908ce8e218326cbb
clarapenn <49952437+clarapenn@users.noreply.github.com>
2024-04-26 16:47:13 +0100

    Pull in data from trivia API and output it to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import { useState } from 'react'
 import React from "react"
 import Splash from "./components/Splash"
 
 
 export default function App() {
 
+  const [quizData, setQuizData] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://opentdb.com/api.php?amount=5');
+        const data = await response.json();
+        setQuizData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []); // not watching any variables, so only runs when we call it on the prev line
+
   return (
     <main className="container">
-      <Splash />
+      {/* <Splash />*/}
+
+      {/* TODO: move the below into a Quiz component, with the data passed in a prop */}
+      {quizData === null ? (
+        <p>Loading data...</p>
+      ) : (
+        <pre>{JSON.stringify(quizData, null, 2)}</pre> // For now just show we have the data
+      )}
 
     </main>
   )
 }
 
+
+
+
+
+
+
+
+// const [questionsAndAnswers, setQuestionsAndAnswers] = React.useState(() =>
+
+//       fetch(apiUrl).then(res => res.json()).then(data => data.results)
+
+//   )
