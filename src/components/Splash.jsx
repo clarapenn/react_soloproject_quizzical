b3a3953ccf8ada9e91270ce33642ed5323feabcc
clarapenn <49952437+clarapenn@users.noreply.github.com>
2024-05-10 12:48:48 +0100

    Adds click to start button in Splash

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 
-export default function Splash() {
+export default function Splash(props) {
     return (
         <div className="splash">
             <h1>Quizzical</h1>
             <div className="splash--subhead">
                 <h2>The ultimate trivia game</h2>
-                <button className="startButton">Start game</button>
+                <button className="startButton" onClick={props.setNewQuiz}>
+                    Start game
+                </button>
             </div>
         </div>
     )
